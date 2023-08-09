5081fc40da3c7eaeadadc940fb588965d495db8f
[akif ödül] <[akifodul@hotmail.com]>
2023-08-09 08:56:28 +0300

    first

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,38 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        int mesafe, yas, yolculukTipi;
+        double fiyat;
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        Scanner input = new Scanner(System.in);
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        System.out.print("Mesafeyi KM türünden giriniz: ");
+        mesafe = input.nextInt();
+
+        System.out.print("Yaşınızı giriniz: ");
+        yas = input.nextInt();
+
+        System.out.print("Yolculuk tipini giriniz (1 => Tek Yön, 2 => Gidiş Dönüş) :");
+        yolculukTipi = input.nextInt();
+
+        fiyat = mesafe * 0.10;
+
+        if (mesafe <= 0 || yas <= 0 || (yolculukTipi != 1 && yolculukTipi != 2)) {
+            System.out.print("Hatalı veri girdiniz!");
+        } else {
+            if (yas < 12) {
+               fiyat *= 0.5;
+            } else if (yas >= 12 && yas <= 24) {
+                fiyat *= 0.9;
+            } else if (yas >=65) {
+                fiyat *= 0.7;
+            }
         }
+
+        if (yolculukTipi == 2) {
+            fiyat *= 1.8;
+        }
+
+        System.out.print("Toplam Tutar: " +fiyat + " TL");
     }
 }
